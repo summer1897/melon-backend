bd8bdd9fed15929c5555f0b4e57b030c0ce0ea35
duhao <406507191@qq.com>
2018-05-22 18:10:24 +0800

    @dev add resume IResumeManager interface to access all personal resume information

---
+++ user-management-module/src/main/java/com/solstice/melon/manager/dto/WorkExperienceDto.java
@@ -0,0 +1,118 @@
+package com.solstice.melon.manager.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Intellij IDEA
+ *
+ * @Projcet melon
+ * @Author solstice
+ * @Date 2018/5/22
+ * @Time 17:47
+ * @Description
+ */
+public class WorkExperienceDto implements Serializable {
+    private static final long serialVersionUID = 8608111059330716189L;
+
+    private Long id;
+    /**
+     * 工作所在公司或单位
+     */
+    private String company;
+    /**
+     * 工作地址
+     */
+    private String address;
+    /**
+     * 工作职位
+     */
+    private String jobPosition;
+    /**
+     * 工作职位描述
+     */
+    private String description;
+    /**
+     * 证明人
+     */
+    private String reterence;
+    /**
+     * 工作起始时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
+    /**
+     * 工作结束时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJobPosition() {
+        return jobPosition;
+    }
+
+    public void setJobPosition(String jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReterence() {
+        return reterence;
+    }
+
+    public void setReterence(String reterence) {
+        this.reterence = reterence;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
